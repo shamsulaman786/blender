9ddb9e58de038a34cf3e1cebad7d33ef3ea3736b
Shamsul Haque <shamsul@onetobeam.com>
2021-09-07 09:19:44 +0530

    Add ceil & floor skirt using profile

--- Hall_Maker/__init__.py
+++ Hall_Maker/__init__.py
@@ -12,7 +12,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 bl_info = {
-    "name" : "Hall Maker",
+    "name" : "3d Hall Maker",
     "author" : "Shamsul Haque",
     "description" : "",
     "blender" : (2, 80, 0),
--- Hall_Maker/otb_op.py
+++ Hall_Maker/otb_op.py
@@ -53,6 +53,7 @@ class OTB_OT_Apply_All_Op(Operator):
 
         # scale plane
         bpy.ops.transform.resize(value=(50, 50, 50), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True,use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
 
         # perform boolean operation
         bpy.ops.object.mode_set(mode='OBJECT')
@@ -101,48 +102,118 @@ class OTB_OT_Apply_All_Op(Operator):
         wall_object.modifiers["Solidify"].solidify_mode = 'NON_MANIFOLD'
         # bpy.ops.object.modifier_apply()
 
+        # creating skirt
+        # add plane
+        bpy.ops.mesh.primitive_plane_add(enter_editmode=False, align='WORLD', location=(0, 0, 1.5), scale=(1, 1, 1))
+
+        # scale plane
+        bpy.ops.transform.resize(value=(50, 50, 50), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True,use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+
+        # perform boolean operation
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.modifier_add(type='BOOLEAN')
+        extra_plane = context.view_layer.objects.active
+        extra_plane.modifiers["Boolean"].object = wall_object
+        # extra_plane.modifiers["Boolean"].solver = 'FAST'
+        bpy.ops.object.modifier_apply(modifier="Boolean")
+
+        # delete vertices
+        # make sure bmesh is imported
+        bpy.ops.object.mode_set(mode='EDIT')
+        me = extra_plane.data
+        bm = bmesh.from_edit_mesh(me)
+
+        for i, v in enumerate(bm.verts):
+            v.select = i < 4
+
+        bmesh.update_edit_mesh(me)
+        bpy.ops.mesh.delete(type='VERT')
+
+
+        #naming based on no of halls
+        halls = 1
+        for obj in bpy.data.objects:
+            if 'Walls' in obj.name:
+                halls += 1
+
+        extra_plane.name = str(halls) + '.Floor Skirt'  # object rename
+        extra_plane.data.name = str(halls) + '.Floor Skirt'  # mesh rename
+        skirt_object = bpy.data.objects['1.Floor Skirt']
+        bpy.ops.object.editmode_toggle()
+        bpy.ops.object.convert(target='CURVE')
+        bpy.context.view_layer.objects.active = bpy.data.objects['1.Floor Skirt']
+        bpy.ops.transform.translate(value=(-0, -0, -1.5), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0, 0, 3.0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, True), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
+        bpy.data.objects['1.Floor Skirt.001'].name = '1.Ceil Skirt'
+      
+        file_path = 'skirt.blend'
+        inner_path = "Curve"
+        floor_profile_obj = 'floor_profile'
+        ceil_profile_obj = 'ceil_profile'
+        
+        bpy.ops.wm.append(
+            filepath=os.path.join(file_path, inner_path, floor_profile_obj),
+            directory=os.path.join(file_path, inner_path),
+            filename=floor_profile_obj
+            )
+        bpy.ops.wm.append(
+            filepath=os.path.join(file_path, inner_path, ceil_profile_obj),
+            directory=os.path.join(file_path, inner_path),
+            filename=ceil_profile_obj
+            )
         # naming walls
         wall_object.name = str(halls) + '.Walls'
         wall_object.data.name = str(halls) + '.Walls'
         bpy.data.objects[ceil_object.data.name].select_set(False)
         bpy.data.objects[wall_object.data.name].select_set(True)
         
-
-        bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0, 0, 0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, False), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
-        bpy.ops.transform.resize(value=(1, 1, 0.03), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
-        bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
-        skirt_obj = bpy.data.objects[wall_object.data.name+".001"]
-        skirt_obj.name = str(halls)+".Floor Skirt"
-        bpy.context.view_layer.objects.active = skirt_obj
-
-        # # # add thickness
-        skirt_obj.modifiers["Solidify"].thickness = 0.25
-        skirt_obj.modifiers["Solidify"].offset = 0
-        skirt_obj.modifiers["Solidify"].solidify_mode = 'NON_MANIFOLD'
-
-        for obj in [skirt_obj,wall_object]:
-            bpy.context.view_layer.objects.active = obj
-            bpy.ops.object.modifier_apply(modifier="Solidify")
-        bpy.context.view_layer.objects.active = wall_object
-
-        bpy.ops.object.modifier_add(type='BOOLEAN')
-        bpy.context.object.modifiers["Boolean"].operation = 'UNION'
-        bpy.context.object.modifiers["Boolean"].object = bpy.data.objects[skirt_obj.name]
-        bpy.ops.object.modifier_apply(modifier="Boolean")
-
         #materialising 
-        bpy.ops.object.mode_set(mode='EDIT')
+        # bpy.ops.object.mode_set(mode='EDIT')
 
         root_dir = "C:\\Program Files\\Blender Foundation\\Blender 2.93\\2.93\\scripts\\addons\\otb\\"
         floor = {"Base Color": "", "Roughness": "", "Normal": "", "dir": ""}
-        ceil = {"Base Color": "", "Roughness": "", "Normal": "", "dir": ""}
-        wall = {"Base Color": "", "Roughness": "", "Normal": "", "dir": ""}
+        # ceil = {"Normal": "", "dir": ""}
+        # wall = {"Normal": "", "dir": ""}
+        ceil = {"Base Color": "","Specular": "", "Normal": "", "dir": ""}
+        wall = {"Base Color": "","Specular": "", "Normal": "", "dir": ""}
 
-        objects = {floor_object.name: floor, ceil_object.name: ceil, wall_object.name: wall}
+        texObjects = {floor_object.name: floor, ceil_object.name: ceil, wall_object.name: wall}
+        matObjects = {floor_object.name: floor, ceil_object.name: ceil, wall_object.name: wall}
         self.init(root_dir, floor, ceil, wall)
-        self.setTextureImage(objects)
-        self.setMaterial(objects,halls)
+        self.setTextureImage(texObjects)
+        self.setMaterial(matObjects,halls)
+        self.doSkirting()
         return {'FINISHED'}
+
+    def doSkirting(self):
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.view_layer.objects.active = bpy.data.objects['1.Floor Skirt']        # bpy.context.space_data.context = 'DATA'
+        # bpy.data.objects['1.Floor Skirt'].select_set(True)
+        bpy.context.object.data.bevel_mode = 'OBJECT'
+        bpy.context.object.data.bevel_object = bpy.data.objects["floor_profile"]
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.objects['floor_profile'].select_set(True)
+        bpy.ops.transform.resize(value=(0.01, 0.01, 0.01), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.view_layer.objects.active = bpy.data.objects['1.Ceil Skirt']        # bpy.context.space_data.context = 'DATA'
+        # bpy.data.objects['1.Ceil Skirt'].select_set(True)
+        bpy.context.object.data.bevel_mode = 'OBJECT'
+        bpy.context.object.data.bevel_object = bpy.data.objects["ceil_profile"]
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.objects['ceil_profile'].select_set(True)
+        bpy.ops.transform.resize(value=(100, 100, 100), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+
+        # bpy.context.view_layer.objects.active = bpy.data.objects['floor_profile']
+
+        # bpy.ops.transform.resize(value=(0.001, 0.001, 0.001), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        # bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+        # bpy.context.view_layer.objects.active = bpy.data.objects['ceil_profile']
+        # bpy.ops.transform.resize(value=(0.001, 0.001, 0.001), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        # bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+        pass
+
 
     def init(self,root_dir, floor, ceil, wall):
         for file in glob.glob(root_dir + '*wall*'):
@@ -156,20 +227,30 @@ class OTB_OT_Apply_All_Op(Operator):
         for file in glob.glob(root_dir + '*floor*'):
             # global floor_dir
             floor['dir'] = file + "\\"
+            
     def setTextureImage(self,objects):
         for obname, ob in objects.items():
-            for file in glob.glob(ob["dir"]+"*color*"):
-                ob['Base Color'] = file
-            for file in glob.glob(ob["dir"]+"*rough*"):
-                ob['Roughness'] = file
             for file in glob.glob(ob["dir"]+"*norm*"):
                 ob['Normal'] = file
+
+            for file in glob.glob(ob["dir"]+"*color*"):
+                ob['Base Color'] = file
+            if ob['Base Color'] == "":
+                for file in glob.glob(ob["dir"]+"*diffuse*"):
+                    ob['Base Color'] = file
+            for file in glob.glob(ob["dir"]+"*rough*"):
+                ob['Roughness'] = file
+            if obname.find('Floor')>=0:
+                continue
+            for file in glob.glob(ob["dir"]+"*specular*"):
+                ob['Specular'] = file
 
     def setMaterial(self,objects,halls):
         for obname, ob in objects.items():
             # print(ob)
             ob_mat = bpy.data.materials.new(name=obname+"_mat")
             ob_mat.use_nodes = True
+            bsdf = ob_mat.node_tree.nodes["Principled BSDF"]
             if obname == str(halls) + '.Floor':
                 value_shader_node = ob_mat.node_tree.nodes.new('ShaderNodeValue')
                 tex_cord_shader_node = ob_mat.node_tree.nodes.new(
@@ -181,47 +262,35 @@ class OTB_OT_Apply_All_Op(Operator):
                     value_shader_node.outputs['Value'], mapping_shader_node.inputs['Scale'])
                 ob_mat.node_tree.links.new(
                     tex_cord_shader_node.outputs['UV'], mapping_shader_node.inputs['Vector'])
-            elif obname == str(halls) + '.Walls':
+            else:
                 wall_mat = bpy.data.materials.new(name=obname+"_mat")
                 wall_mat.use_nodes = True
-
-                # wall_mat = bpy.context.object.data.materials['Walls_mat']
+                wall_mat = ob_mat
+                
                 texCordNode = wall_mat.node_tree.nodes.new('ShaderNodeTexCoord')
-                fNoiseTexNode = wall_mat.node_tree.nodes.new('ShaderNodeTexNoise')
-                sNoiseTexNode = wall_mat.node_tree.nodes.new('ShaderNodeTexNoise')
-                wall_mat.node_tree.links.new(texCordNode.outputs['Object'],fNoiseTexNode.inputs['Vector'])
-                wall_mat.node_tree.links.new(texCordNode.outputs['Object'], sNoiseTexNode.inputs['Vector'])
-                fNoiseTexNode.inputs['Scale'].default_value = 1.5
-                fNoiseTexNode.inputs['Roughness'].default_value = .383
-                sNoiseTexNode.inputs['Scale'].default_value = 400
-                sNoiseTexNode.inputs['Roughness'].default_value = .717
+                valNode = wall_mat.node_tree.nodes.new('ShaderNodeValue')
+                valNode.outputs[0].default_value=10
+                # valNode = wall_mat.node_tree.nodes["Value"].outputs[0].default_value = 10
+                mapping_shader_node = ob_mat.node_tree.nodes.new(
+                    'ShaderNodeMapping')
+                ob_mat.node_tree.links.new(texCordNode.outputs['UV'], mapping_shader_node.inputs['Vector'])
+                ob_mat.node_tree.links.new(valNode.outputs['Value'], mapping_shader_node.inputs['Scale'])
 
                 fColorRamp = wall_mat.node_tree.nodes.new('ShaderNodeValToRGB')
                 fColorRamp.color_ramp.elements[0].position= .333
                 fColorRamp.color_ramp.elements[0].color = (0.517455, 0.517455, 0.517455, 1)
-                wall_mat.node_tree.links.new(fNoiseTexNode.outputs['Fac'], fColorRamp.inputs['Fac'])
+                # baseColor = wall_mat.node_tree.nodes['Image Texture']
+                wall_mat.node_tree.links.new(fColorRamp.outputs['Color'], bsdf.inputs['Base Color'])
 
-                sColorRamp = wall_mat.node_tree.nodes.new('ShaderNodeValToRGB')
-                sColorRamp.color_ramp.elements[0].position= .352
-
-                wall_mat.node_tree.links.new(sNoiseTexNode.outputs['Fac'], sColorRamp.inputs['Fac'])
-
-                # principledBsdfNode = wall_mat.node_tree.nodes.new["ShaderNodeBsdfPrincipled"]
-                principledBsdfNode = wall_mat.node_tree.nodes['Principled BSDF']
-                wall_mat.node_tree.links.new(fColorRamp.outputs['Color'], principledBsdfNode.inputs['Roughness'])
-
-                bumpNode = wall_mat.node_tree.nodes.new('ShaderNodeBump')
-                bumpNode.inputs['Strength'].default_value= .35
-
-                wall_mat.node_tree.links.new(sColorRamp.outputs['Color'], bumpNode.inputs['Normal'])
-                wall_mat.node_tree.links.new(bumpNode.outputs['Normal'], principledBsdfNode.inputs['Normal'])
-
-            bsdf = ob_mat.node_tree.nodes["Principled BSDF"]
             for texImgName, texImgAddr in ob.items():
-                if obname == str(halls) + '.Walls' or (texImgName == "dir"):
+                if (texImgName == "dir"):
                     continue
                 texImage = ob_mat.node_tree.nodes.new('ShaderNodeTexImage')
                 texImage.image = bpy.data.images.load(texImgAddr)
+                if (obname != str(halls) + '.Floor' and texImgName == "Base Color"):
+                    ob_mat.node_tree.links.new(texImage.outputs['Color'], fColorRamp.inputs['Fac'])
+                    ob_mat.node_tree.links.new(fColorRamp.outputs['Color'], bsdf.inputs['Base Color'])
+
                 if texImgName == 'Normal':
                     normalMapTex = ob_mat.node_tree.nodes.new(
                         'ShaderNodeNormalMap')
@@ -229,12 +298,13 @@ class OTB_OT_Apply_All_Op(Operator):
                         normalMapTex.inputs['Color'], texImage.outputs['Color'])
                     ob_mat.node_tree.links.new(
                         bsdf.inputs[texImgName], normalMapTex.outputs[texImgName])
-                else:
+                elif (texImgName == 'Specular' and obname != str(halls) + '.Floor') or obname == str(halls) + '.Floor':
                     ob_mat.node_tree.links.new(
                         bsdf.inputs[texImgName], texImage.outputs['Color'])
-                if obname == str(halls) + '.Floor':
-                    ob_mat.node_tree.links.new(
+                ob_mat.node_tree.links.new(
                         mapping_shader_node.outputs['Vector'], texImage.inputs['Vector'])
+
+
             curOb = bpy.data.objects[obname]
             # Assign it to object
             if curOb.data.materials:
